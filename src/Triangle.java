4013399a02f8f27bd651df518b61c41760c75e93
pluviophileAkiko <aqilahrais14@gmail.com>
2016-06-20 13:32:41 +0800

    side4

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,13 +2,13 @@
  * Created by kaito on 17/6/16.
  */
 public class Triangle {
-    double side1 = 1.0,side2 = 1.0,side3 = 1.0;
+    double side1 = 1.0,side2 = 1.0,side4 = 1.0;
     Triangle(double newT1, double newT2, double newT3) {
         side1 = newT1;
         side2 = newT2;
-        side3 = newT3;
+        side4 = newT3;
     }
     public double getPerimeter() {
-        return side1 + side2 + side3;
+        return side1 + side2 + side4;
     }
 }
